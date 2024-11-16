64ada03708a69b9f1d5e3f0a9199cbefca4ce465
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-11-17 01:57:05 +0900

    chore: format

--- build.ts
+++ build.ts
@@ -1,9 +1,9 @@
 import { existsSync, rmSync } from "node:fs";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
-import esbuild from "esbuild"
+import esbuild from "esbuild";
 
-const rootDir = resolve(dirname(fileURLToPath(import.meta.url)), './');
+const rootDir = resolve(dirname(fileURLToPath(import.meta.url)), "./");
 
 const outDir = resolve(rootDir, "dist");
 const isExist = existsSync(outDir);
@@ -13,9 +13,7 @@ if (isExist) {
 
 await esbuild.build({
   platform: "node",
-  entryPoints: [
-    resolve(rootDir, 'src/main.ts'),
-  ],
+  entryPoints: [resolve(rootDir, "src/main.ts")],
   outdir: outDir,
   bundle: true,
   treeShaking: true,
