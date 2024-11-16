11753986fc9a55a51a523229fbda91b066bb235e
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-11-17 01:49:32 +0900

    chore: setting build script

---
+++ build.ts
@@ -0,0 +1,22 @@
+import { existsSync, rmSync } from "node:fs";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import esbuild from "esbuild"
+
+const rootDir = resolve(dirname(fileURLToPath(import.meta.url)), './');
+
+const outDir = resolve(rootDir, "dist");
+const isExist = existsSync(outDir);
+if (isExist) {
+  rmSync(outDir, { recursive: true, force: true });
+}
+
+await esbuild.build({
+  platform: "node",
+  entryPoints: [
+    resolve(rootDir, 'src/main.ts'),
+  ],
+  outdir: outDir,
+  bundle: true,
+  treeShaking: true,
+});
