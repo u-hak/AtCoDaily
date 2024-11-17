41fdec918d484f4796b10d5fb4e5ab58c2bd3469
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-11-17 23:16:55 +0900

    feat: minimum bot setup

--- src/main.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "vitest";
-import { sum } from "./main.ts";
-
-test("Just test", () => {
-  expect(sum(1, 2)).toBe(3);
-});
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,45 @@
-export const sum = (a: number, b: number) => {
-  return a + b;
-};
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+import { Effect } from "effect";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildScheduledEvents,
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Reaction,
+    Partials.Message,
+    Partials.GuildMember,
+    Partials.GuildScheduledEvent,
+    Partials.User,
+  ],
+});
+
+client.on("ready", (c) => {
+  console.log(`Bot is logged in as ${c.user.tag}`);
+});
+
+const getToken = () =>
+  Effect.gen(function* () {
+    const token = process.env.DISCORD_TOKEN;
+    if (!token) {
+      return yield* Effect.fail(
+        new Error("DISCORD_TOKEN is not setting in env. vars"),
+      );
+    }
+    return yield* Effect.succeed(token);
+  });
+
+const login = (token: string) => Effect.tryPromise(() => client.login(token));
+
+const main = Effect.gen(function* () {
+  const token = yield* getToken();
+  return yield* login(token);
+});
+
+Effect.runPromise(main);
